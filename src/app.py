6f6fe6aae866d3858d625d3b676f7fd39cbc821b
Olga Chertkova <ochertkova2019@gmail.com>
2022-07-25 17:30:04 +0300

    Restructure deps dictionary Add all local commits to a new branch at GitHub

---
+++ src/app.py
@@ -0,0 +1,28 @@
+from distutils.log import error
+import os
+from flask import *
+import store
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html", deps=store.get_dep_names())
+
+@app.route("/<name>")
+def dep_info(name):
+    try:
+        dep_in_store = store.find_dep_by_name(name)
+        print("dep in store found")
+        #print(dep_in_store.keys())
+        return render_template("dependency.html", dep_to_render=dep_in_store)
+    except:
+        abort(404)
+    
+    #return render_template("dependency.html", dep=store.find_dep_by_name(name))
+
+    #return "The product is " + ", ".join(store.find_dep_by_name(name).keys())
+
+if __name__ == "__main__": 
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False,port=port)
---
+++ src/store.py
@@ -0,0 +1,40 @@
+from poetry_deps import get_test_data
+def find_rev_deps(name):
+    return [1, 2, 3]
+ 
+def process_raw_data(raw_data):
+    """Sort deps by name, add reverse dependancies to dictionary"""
+    for elem in raw_data:
+        elem['rev_deps'] = find_rev_deps(elem['name'])
+    
+    data_sorted = sorted(raw_data, key=lambda d: d['name'])
+    return data_sorted #???list of dictionaries
+
+deps = process_raw_data(get_test_data()) #map object
+
+def find_dep_by_name(name):
+    """Find dependency by name and return a dep-dictionary"""
+    for dep in iter(deps): 
+        if dep['name'] == name:
+            return dep
+    return None
+
+def get_dep_names():
+    """Get a list of dependancy names for representation"""
+    dep_names = []
+    for dep in iter(deps):
+        dep_names.append(dep['name'])
+    return dep_names
+
+def get_opt_dep_names(name = ''):
+    """Get a list of optional dependancy names for representation in html Dependancy page"""
+    opt_dep_names = []
+    for dep in iter(deps):
+        if dep['name'] == name:
+            for d in dep['optional_deps']:
+                opt_dep_names.append(d)
+    #print(opt_dep_names)
+    return opt_dep_names
+
+# dep_names = get_dep_names(index_page = False, name = 'cachecontrol')
+# opt_dep_names = get_opt_dep_names('cachecontrol')
